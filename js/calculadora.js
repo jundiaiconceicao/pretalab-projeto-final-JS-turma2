5252524cd298ccae897a7a3479ddf61e04f9c356
Jundiai Conceicao <primllh@gmail.com>
2023-12-08 17:05:35 -0300

    primeiro commit

--- js/calculadora.js
+++ js/calculadora.js
@@ -4,17 +4,63 @@
 */
 
 function somar() {
+    const numero1 = parseFloat(document.querySelector("#numero1Soma").value)
+    const numero2 = parseFloat(document.querySelector("#numero2Soma").value)
+
+    const total = numero1 + numero2
+
+    document.querySelector(".resultado").setAttribute('id', 'resultadoSoma')
+    
+
+    const paragrafo = document.getElementById("resultadoSoma")
+    const resultado = document.createElement("strong")
+    paragrafo.appendChild(resultado)
+    resultado.innerText = total
 
 }
 
 function subtrair() {
+    const numero1 = parseFloat(document.querySelector("#numero1Sub").value)
+    const numero2 = parseFloat(document.querySelector("#numero2Sub").value)
 
-}
+    const total = numero1 - numero2
+
+   
+    
+
+    const paragrafo = document.getElementById("resultadoSub")
+    const resultado = document.createElement("strong")
+    paragrafo.appendChild(resultado)
+    resultado.innerText = total
+    }
 
 function multiplicar() {
+    const numero1 = parseFloat(document.querySelector("#numero1Mult").value)
+    const numero2 = parseFloat(document.querySelector("#numero2Mult").value)
 
+    const total = numero1 * numero2
+
+    
+    
+
+    const paragrafo = document.getElementById("resultadoMult")
+    const resultado = document.createElement("strong")
+    paragrafo.appendChild(resultado)
+    resultado.innerText = total
 }
 
 function dividir() {
+    const numero1 = parseFloat(document.querySelector("#numero1Div").value)
+    const numero2 = parseFloat(document.querySelector("#numero2Div").value)
+
+    const total = numero1 / numero2
+
+  
+    
+
+    const paragrafo = document.getElementById("resultadoDiv")
+    const resultado = document.createElement("strong")
+    paragrafo.appendChild(resultado)
+    resultado.innerText = total
 
 }
